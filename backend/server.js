b7df8a8a6b78f14e88296f1007d94eadbdc03103
Devanshi Awasthi <2k23.csaiml2313992@gmail.com>
2026-05-18 19:38:09 +0530

    Production Ready

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,10 @@ require('dotenv').config();
 const goalRoutes = require('./routes/goalRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const portalRoutes = require('./routes/portalRoutes');
-
+const path = require('path')
 const app = express();
 
+const dir = path.resolve();
 app.use(cors());
 app.use(express.json());
 
@@ -19,6 +20,16 @@ app.use('/api/portal', portalRoutes);
 const PORT = process.env.PORT || 5001;
 const HOST = process.env.HOST || '127.0.0.1';
 
+app.use(express.static(path.join(dir , "/frontend/dist")))
+app.get(/_/,(_,res)=>{
+  res.sendFile(path.resolve(dir,"frontend" , "dist" , "index.html"));
+})
+app.use((req, res) => {
+    res.status(404).json({ 
+        success: false,
+        message: "Endpoint not found on AtomAlign API" 
+    });
+});
 app.listen(PORT, HOST, () => {
   console.log(`Server running on http://${HOST}:${PORT}`);
 });
